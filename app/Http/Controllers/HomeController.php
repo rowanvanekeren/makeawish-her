61936027d0bd881dfb0b08698f2542b7fbbf3404
rowanvanekeren <rowanvanekeren@hotmail.com>
2016-12-19 12:31:34 +0100

    last 3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Wish;
 class HomeController extends Controller
 {
     /**
@@ -23,7 +23,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('index');
+        $lastWishes = Wish::orderBy('created_at','DESC')->take(3)->get();
+
+        return view('index', ['wishes' => $lastWishes]);
     }
 
     public function bridge() {
