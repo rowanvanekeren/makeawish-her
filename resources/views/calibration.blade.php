0e226525cd130fef143c19b05d9f8c6ac24f3cfd
rowanvanekeren <rowanvanekeren@hotmail.com>
2017-01-05 22:52:57 +0100

    calibration page

--- resources/views/calibration.blade.php
+++ resources/views/calibration.blade.php
@@ -1,73 +1,74 @@
 @extends('layouts.layout')
 
 @section('content')
-    <style>
-.landerditmoetjijverwijderen {
-    background-color: grey;
-}
-    .preset_ul li{
-        display:inline-block;
-    }
 
-    </style>
-    <div class="landerditmoetjijverwijderen" ng-controller="micStreamAngController" ng-init="initCalibrate()">
-       <div>
-           <a href="{{ url('/logout') }}"
-              onclick="event.preventDefault();
+    <div class="container" ng-controller="micStreamAngController" ng-init="initCalibrate()">
+        <div class="inner-container">
+            <div class="custom-style">
+                <a href="{{ url('/logout') }}"
+                   onclick="event.preventDefault();
               document.getElementById('logout-form').submit();">
-               Logout
-           </a>
-           <form id="logout-form" action="{{ url('/logout') }}" method="POST">
-               {{ csrf_field() }}
-           </form>
-           @if(isset($currCookie[1]))
-           <h1 >  {{ $currCookie[1] }}</h1>
-           @else
-               <h1>No preset selected</h1>
-           @endif
-        @if(isset($presets))
-            @foreach($presets as $preset)
-                <div>
+                    Logout
+                </a>
 
-                    <ul class="preset_ul">
-
-                    <li>{{$preset->name}}</li>
-                    <li>{{$preset->max}}</li>
-                        <li>
-                    <form method="POST" action="./choosepreset">
+                <form id="logout-form" action="{{ url('/logout') }}" method="POST">
                     {{ csrf_field() }}
-                    <input type="hidden" name="preset_name" value="{{$preset->name}}"/>
-                    <input type="hidden" name="preset_max" value="{{$preset->max}}"/>
-                    <input type="submit" name="submit" value="Choose"/>
-                    </form>
-                        </li>
-                        <li>
-                    <form method="POST" action="./deletepreset">
-                        {{ csrf_field() }}
-                        <input type="hidden" name="delete_id" value="{{$preset->id}}"/>
-                        <input type="submit" name="submit" value="Delete"/>
-                    </form>
-                        </li>
-                    </ul>
+                </form>
+
+
+                @if(isset($currCookie[1]))
+                   <h1 class="preset-curr">{{ $currCookie[0] }} - {{ $currCookie[1] }}</h1>
+                @else
+                    <h1 class="preset-curr">No preset selected</h1>
+                @endif
+                <div class="preset-container">
+                    <table>
+                    @if(isset($presets))
+                        @foreach($presets as $preset)
+
+
+
+                        <tr>
+                                    <td class="preset-name"><div>{{$preset->name}}</div></td>
+                                    <td  class="preset-value"><div>{{$preset->max}}</div></td>
+                                    <td >
+                                        <form method="POST" action="./choosepreset">
+                                            {{ csrf_field() }}
+                                            <input type="hidden" name="preset_name" value="{{$preset->name}}"/>
+                                            <input type="hidden" name="preset_max" value="{{$preset->max}}"/>
+                                            <input class="button-3d choose" type="submit" name="submit" value="Choose"/>
+                                        </form>
+                                    </td>
+                                    <td>
+                                        <form method="POST" action="./deletepreset">
+                                            {{ csrf_field() }}
+                                            <input type="hidden" name="delete_id" value="{{$preset->id}}"/>
+                                            <input class="button-3d delete" type="submit" name="submit" value="Delete"/>
+                                        </form>
+                                    </td>
+                        </tr>
+
+
+                        @endforeach
+                    @endif
+                    </table>
                 </div>
+                <div class="preset-new">
+                <form method="POST" action="./savepreset">
+                    {{ csrf_field() }}
+                    <ul>
+                    <li><div><input class="custom-box" type="text" id="preset_name" name="preset_name" placeholder="preset name" required/></div></li>
+                        <li> <div> <input class="custom-box"type="number" id="preset_number" name="preset_number" required/></div></li>
+                        <li> <div> <input class="button-3d" type="submit" name="submit" value="Save Preset"/></div></li>
+                    </ul>
+                </form>
+                </div>
+                <div class="preset-calibrate">
+                    <button class="button-3d" ng-click="calibration()">Calibrate mic device</button>
 
-            @endforeach
-        @endif
-
-       </div>
-       <form method="POST" action="./savepreset">
-           {{ csrf_field() }}
-           <input type="text" id="preset_name" name="preset_name" placeholder="preset name" required/>
-           <input type="number" id="preset_number" name="preset_number" required/>
-           <input type="submit" name="submit"/>
-       </form>
-
-        <button ng-click="calibration()">Calibrate mic device</button>
-
-        <h1 style="color:white" id="calCounter"></h1>
+                        <h1 id="calCounter"></h1>
+                </div>
+            </div>
+        </div>
     </div>
-@endsection
-
-@section('scripts')
-   {{-- <script src="{{url('/js/calibrate.js')}}"></script>--}}
 @endsection
