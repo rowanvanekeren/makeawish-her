eb6a9b9afee08773a162e27bfb0c49eeca980b3a
LanderVerschueren <verschueren@live.nl>
2017-01-15 14:45:50 +0100

    arrow

--- resources/views/wish.blade.php
+++ resources/views/wish.blade.php
@@ -85,7 +85,7 @@ $preset_error = $general_errors->general_errors('cookiePreset');
     --}}
 
     <div class="wish-blow" ng-controller="micStreamAngController" ng-init="initWish()" ng-show="blowingEnabled">
-        <img src="/images/upload-2.svg" alt="">
+        <img src="../images/upload-2.svg" alt="">
         <div class="blowdiv">
             <div class="blowdiv-inner" style="background-image: url( {{asset('/images/tekstballon-2.png')}} )">
                 <h2>@{{ wishText }}</h2>
