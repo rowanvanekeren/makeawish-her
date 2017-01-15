ed34f478d5f52bebbac071a8b4d4acf951bde8a8
LanderVerschueren <verschueren@live.nl>
2017-01-15 19:15:05 +0100

    background + white icons

--- resources/views/layouts/wish.blade.php
+++ resources/views/layouts/wish.blade.php
@@ -21,7 +21,7 @@
         ]); ?>
     </script>
 </head>
-<body ng-app="blowawish">
+<body ng-app="blowawish" style="background-image: url({{asset('images/background.jpg')}})">>
 
 	
 	@yield('content')
